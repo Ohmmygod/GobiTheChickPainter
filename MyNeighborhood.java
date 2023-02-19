d2bdf19c25e4f089ee212804f4985915fa65db12
Ohmmygod <102958249+Ohmmygod@users.noreply.github.com>
2023-02-20 06:53:35 +0700

    Create MyNeighborhood.java

---
+++ MyNeighborhood.java
@@ -0,0 +1,46 @@
+import org.code.neighborhood.Painter;
+
+public class MyNeighborhood {
+  public static void main(String[] args) {
+
+
+    GobiTheChickPainter backgroundPainter = new GobiTheChickPainter();
+    backgroundPainter.setPaint(1000);
+    backgroundPainter.paintBackground();  
+    GobiTheChickPainter oxmmsy = new GobiTheChickPainter();
+    oxmmsy.setPaint(1000);
+    oxmmsy.paintFirst();
+    oxmmsy.paintSecond("brown");
+    oxmmsy.paintThird();
+    oxmmsy.paintFourth();
+    oxmmsy.paintFifth();
+    oxmmsy.paintSixth();
+    oxmmsy.paintSeventh();
+    oxmmsy.paintEighth();
+    oxmmsy.paintNinth();
+    oxmmsy.paintTenth();
+    oxmmsy.paintEleventh();
+    oxmmsy.paintTwelfth();
+    oxmmsy.paintThirteenth();
+    oxmmsy.paintFourteenth();
+    oxmmsy.paintFifteenth();
+    oxmmsy.paintSixteenth();
+    oxmmsy.paintSeventeenth();
+    oxmmsy.paintEightteenth();
+    oxmmsy.paintNineteenth();
+    oxmmsy.paintTwentieth();
+    oxmmsy.paintTwentyFirst();
+    oxmmsy.paintTwentySecond();
+    oxmmsy.paintTwentyThird();
+    oxmmsy.paintTwentyFourth();
+    oxmmsy.paintTwentyFifth();
+    oxmmsy.paintTwentySixth();
+    oxmmsy.paintTwentySeventh();
+    oxmmsy.paintTwentyEighth();
+    oxmmsy.paintTwentyNinth("brown");
+    oxmmsy.turnRight();
+    oxmmsy.backToBegin();
+    oxmmsy.turnRight();
+    
+  }
+}
